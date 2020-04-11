f572d3c548b21b3af78e05a1f5433d75bd8e3008
DaleFoley <dale.foley@live.com.au>
2020-04-11 16:45:03 +1000

    WIP scrape data

--- ACNHGuide/Scripts/scrape_bugs.py
+++ ACNHGuide/Scripts/scrape_bugs.py
@@ -11,27 +11,84 @@ import re
 path_assets = "../../ACNHGuide.Android/Resources/drawable"
 path_sqlite = "../../app_data.db"
 
+table_name_bugs = 'bugs'
+table_name_northern_months = 'northern_months'
+table_name_southern_months = 'southern_months'
+
 ascii_checkmark = 10003
 
 db_connection = sqlite3.connect(path_sqlite)
 db_command = db_connection.cursor()
 
-db_command.execute('''drop table if exists bugs_northern''')
-db_command.execute('''create table bugs_northern (name text primary key,
+db_command.execute('''drop table if exists ''' + table_name_bugs)
+db_command.execute('''create table ''' + table_name_bugs + ''' (name text primary key,
     sell_price integer,
     location text,
-    time text,
+    time text)''')
+
+db_command.execute('''drop table if exists ''' + table_name_northern_months)
+db_command.execute('''create table ''' + table_name_northern_months + ''' (id integer primary key,
+    animal_name text,
     months text)''')
 
-db_command.execute('''drop table if exists bugs_southern''')
-db_command.execute('''create table bugs_southern (name text primary key,
-    sell_price integer,
-    location text,
-    time text,
+db_command.execute('''drop table if exists ''' + table_name_southern_months)
+db_command.execute('''create table ''' + table_name_southern_months + ''' (id integer primary key,
+    animal_name text,
     months text)''')
 
 
-def insert_bugs(html_content, destination_db_table):
+def insert_animal_months(html_content, destination_table):
+    for idx in range(1, len(html_content)):
+        html_table_row = html_content[idx]
+
+        name = html_table_row.contents[1].text.strip()
+
+        months_available = []
+
+        if ord(html_table_row.contents[6].text.strip()) == ascii_checkmark:
+            months_available.append("jan")
+
+        if ord(html_table_row.contents[7].text.strip()) == ascii_checkmark:
+            months_available.append("feb")
+
+        if ord(html_table_row.contents[8].text.strip()) == ascii_checkmark:
+            months_available.append("mar")
+
+        if ord(html_table_row.contents[9].text.strip()) == ascii_checkmark:
+            months_available.append("apr")
+
+        if ord(html_table_row.contents[10].text.strip()) == ascii_checkmark:
+            months_available.append("may")
+
+        if ord(html_table_row.contents[11].text.strip()) == ascii_checkmark:
+            months_available.append("jun")
+
+        if ord(html_table_row.contents[12].text.strip()) == ascii_checkmark:
+            months_available.append("jul")
+
+        if ord(html_table_row.contents[13].text.strip()) == ascii_checkmark:
+            months_available.append("aug")
+
+        if ord(html_table_row.contents[14].text.strip()) == ascii_checkmark:
+            months_available.append("sep")
+
+        if ord(html_table_row.contents[15].text.strip()) == ascii_checkmark:
+            months_available.append("oct")
+
+        if ord(html_table_row.contents[16].text.strip()) == ascii_checkmark:
+            months_available.append("nov")
+
+        if ord(html_table_row.contents[17].text.strip()) == ascii_checkmark:
+            months_available.append("dec")
+
+        months = ' '.join(months_available)
+        db_command.execute('insert into ' + destination_table + ' (animal_name, months) values (?, ?)',
+                           (name, months))
+
+    db_connection.commit()
+
+
+def insert_bugs(html_content):
     for idx in range(1, len(html_content)):
         bug_row = html_content[idx]
 
@@ -51,47 +108,8 @@ def insert_bugs(html_content, destination_db_table):
             time = time_match[0][0].strip().upper() + time_match[0][1].strip().upper() + \
                    " - " + time_match[1][0].strip().upper() + time_match[1][1].strip().upper()
 
-        months_available = []
-
-        if ord(bug_row.contents[6].text.strip()) == ascii_checkmark:
-            months_available.append("jan")
-
-        if ord(bug_row.contents[7].text.strip()) == ascii_checkmark:
-            months_available.append("feb")
-
-        if ord(bug_row.contents[8].text.strip()) == ascii_checkmark:
-            months_available.append("mar")
-
-        if ord(bug_row.contents[9].text.strip()) == ascii_checkmark:
-            months_available.append("apr")
-
-        if ord(bug_row.contents[10].text.strip()) == ascii_checkmark:
-            months_available.append("may")
-
-        if ord(bug_row.contents[11].text.strip()) == ascii_checkmark:
-            months_available.append("jun")
-
-        if ord(bug_row.contents[12].text.strip()) == ascii_checkmark:
-            months_available.append("jul")
-
-        if ord(bug_row.contents[13].text.strip()) == ascii_checkmark:
-            months_available.append("aug")
-
-        if ord(bug_row.contents[14].text.strip()) == ascii_checkmark:
-            months_available.append("sep")
-
-        if ord(bug_row.contents[15].text.strip()) == ascii_checkmark:
-            months_available.append("oct")
-
-        if ord(bug_row.contents[16].text.strip()) == ascii_checkmark:
-            months_available.append("nov")
-
-        if ord(bug_row.contents[17].text.strip()) == ascii_checkmark:
-            months_available.append("dec")
-
-        months = ' '.join(months_available)
-        db_command.execute('insert into ' + destination_db_table + ' values (?, ?, ?, ?, ?)''',
-                           (name, sell_price, location, time, months))
+        db_command.execute('''insert into bugs values (?, ?, ?, ?)''',
+                           (name, sell_price, location, time))
 
     db_connection.commit()
 
@@ -103,9 +121,10 @@ if response.status_code == 200:
     html_source = BeautifulSoup(html_doc, 'html.parser')
 
     bug_rows_northern = html_source.findAll('table')[2].findAll('table')[0].findAll('tr')
-    insert_bugs(bug_rows_northern, "bugs_northern")
+    insert_bugs(bug_rows_northern)
+    insert_animal_months(bug_rows_northern, table_name_northern_months)
 
     bug_rows_southern = html_source.findAll('table')[4].findAll('table')[0].findAll('tr')
-    insert_bugs(bug_rows_southern, "bugs_southern")
+    insert_animal_months(bug_rows_southern, table_name_southern_months)
 
 db_connection.close()
